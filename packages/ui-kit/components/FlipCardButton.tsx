2d4983fe8684a0e32c86c89d157834e64ac1739c
jellybiens <ethanwatson2603@outlook.com>
2020-12-10 16:57:02 +0000

    feat: flip card pile molecule set up

--- packages/ui-kit/components/FlipCardButton.tsx
+++
@@ -1,4 +0,0 @@
-import * as React from 'react';
-import { Button } from '@material-ui/core';
-
-export const FlipCardButton = () => <Button>asdasdsads</Button>;
--- packages/ui-kit/components/index.ts
+++
@@ -1,1 +0,0 @@
-export { FlipCardButton } from './FlipCardButton';
