e0ade493881a197c380a7cf8213c9a2c5ec3803f
jellybiens <ethanwatson2603@outlook.com>
2021-01-23 14:22:35 +0000

    feat: prep frontend

--- packages/frontend/.eslintrc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  extends: ['react-app', '../../.eslintrc.js', './eslintrc.react.js', 'plugin:cypress/recommended'],
-  rules: {
-    '@typescript-eslint/no-use-before-define': 'off',
-  },
-};
