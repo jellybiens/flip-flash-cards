3ee08af909b996732034e8d85e2a1b12b7f5d0e1
jellybiens <ethanwatson2603@outlook.com>
2021-02-07 00:22:35 +0000

    feat: image cropper and image hook working

--- packages/ui-kit/src/helpers/useCreateImage.tsx
+++ packages/ui-kit/src/helpers/useCreateImage.tsx
@@ -5,34 +5,42 @@ export function useCreateImage(
 ): {
   image: HTMLImageElement;
   loading: boolean;
-  error: ErrorEvent;
+  error: Error;
 } {
   const [loading, setLoading] = React.useState<boolean>(false);
-  const [error, setError] = React.useState<ErrorEvent>(undefined);
+  const [error, setError] = React.useState<Error>(null);
   const [image, setImage] = React.useState<HTMLImageElement>(null);
 
   React.useEffect(() => {
-    setLoading(true);
     let cancel = false;
+    if (url) {
+      setLoading(true);
 
-    new Promise<HTMLImageElement>((resolve, reject) => {
-      const image = new Image();
-      image.setAttribute('crossOrigin', 'anonymous');
-      image.src = url;
-      image.addEventListener('load', () => resolve(image));
-      image.addEventListener('error', (error) => reject(error));
-    }).then(
-      (res) => {
-        if (cancel) return;
-        setLoading(false);
-        setImage(res);
-      },
-      (error) => {
-        if (cancel) return;
-        setLoading(false);
-        setError(error);
-      },
-    );
+      new Promise<HTMLImageElement>((resolve, reject) => {
+        const image = new Image();
+        image.crossOrigin = 'anonymous';
+        image.onload = () => resolve(image);
+        image.onerror = reject;
+        image.src = url;
+      }).then(
+        (res) => {
+          if (cancel) return;
+          setLoading(false);
+          setImage(res);
+          cancel = true;
+        },
+        (error: Error) => {
+          console.log(typeof error);
+          const e = JSON.stringify(error, ['message', 'arguments', 'type', 'name']);
+          const err = JSON.parse(e);
+          console.log(err);
+          if (cancel) return;
+          setLoading(false);
+          setError(err);
+          cancel = true;
+        },
+      );
+    }
     return () => {
       cancel = true;
     };
--- packages/ui-kit/src/molecules/ImageCropper.tsx
+++ packages/ui-kit/src/molecules/ImageCropper.tsx
@@ -1,24 +1,51 @@
 import * as React from 'react';
-import { ButtonBase, Grid, CircularProgress, Typography } from '@material-ui/core';
+import {
+  ButtonBase,
+  Grid,
+  CircularProgress,
+  Typography,
+  makeStyles,
+} from '@material-ui/core';
 import AddImage from '../images/addImage.png';
 import { useCreateImage } from '../helpers';
 import { ImageCropperWindow } from '../atoms/ImageCropperWindow';
 import { Slider } from '../atoms/Slider';
 import { useCropDispatch } from '../context/CroppingContextProvider';
 
-type ImageCropperProps = {
+const useStyles = makeStyles(() => ({
+  buttonContainer: {},
+  buttonWrapper: {
+    height: '50%',
+    width: '50%',
+    transform: 'translate(-50%, -50%)',
+    position: 'relative',
+    top: '50%',
+    left: '50%',
+  },
+  buttonImg: {
+    height: '100%',
+    width: '100%',
+  },
+}));
+
+export interface ImageCropperProps extends React.HTMLProps<HTMLDivElement> {
   src?: string;
-  px: number;
+  px?: number;
   step?: number;
+  min?: number;
+  max?: number;
   handleAddImage: () => void;
-};
+}
 
 export const ImageCropper: React.FC<ImageCropperProps> = ({
   src,
-  px,
+  px = 300,
   step = 0.05,
+  min = 1,
+  max = 2,
   handleAddImage,
 }) => {
+  const cs = useStyles();
   const { image, loading, error } = useCreateImage(src);
 
   const { setImage } = useCropDispatch();
@@ -33,12 +60,19 @@ export const ImageCropper: React.FC<ImageCropperProps> = ({
       <Grid item xs={12}>
         {!!loading && <CircularProgress />}
         {!image && !loading && (
-          <ButtonBase onClick={() => handleAddImage()}>
-            <img src={AddImage} style={{ height: px, width: px }} />
-          </ButtonBase>
+          <div
+            style={{
+              height: px,
+              width: px,
+            }}
+          >
+            <ButtonBase className={cs.buttonWrapper} onClick={() => handleAddImage()}>
+              <img className={cs.buttonImg} src={AddImage} />
+            </ButtonBase>
+          </div>
         )}
         {src &&
-          error && // TODO: Translate
+          !!error && // TODO: Translate
           !loading && (
             <ButtonBase onClick={() => handleAddImage()}>
               <Typography>
@@ -49,8 +83,8 @@ export const ImageCropper: React.FC<ImageCropperProps> = ({
         {!!image && !loading && (
           <ImageCropperWindow
             {...{ px, image, zoom }}
-            zoomIn={() => setZoom(zoom + step > 2 ? 2 : zoom + step)}
-            zoomOut={() => setZoom(zoom - step < 1 ? 1 : zoom - step)}
+            zoomIn={() => setZoom(zoom + step > max ? max : zoom + step)}
+            zoomOut={() => setZoom(zoom - step < min ? min : zoom - step)}
           />
         )}
       </Grid>
@@ -59,8 +93,8 @@ export const ImageCropper: React.FC<ImageCropperProps> = ({
           <Slider
             value={zoom}
             setValue={setZoom}
-            min={1}
-            max={2}
+            min={min}
+            max={max}
             step={step}
             onChange={(_, zoom: number) => setZoom(zoom)}
           />
